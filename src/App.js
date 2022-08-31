43e25faccc8c9709aff02db725f4200b4092d48c
simona <simona.fabbri194@gmail.com>
2022-08-31 20:04:48 +0200

    remove context

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SearchContext from "./SearchContext";
+
 import Home from "./pages/Home";
 import State from "./pages/State";
 
 function App() {
   return (
-    <SearchContext.Provider value={searchQuery}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/:stateCode" element={<State />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </SearchContext.Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/:stateCode" element={<State />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
